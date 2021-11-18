e4a63504d4f614a4c23e26b41749b4bcaf8406dc
Justin Brewer <stan1651@gmail.com>
2021-11-17 18:23:43 -0800

    starter code

---
+++ public/db.js
@@ -0,0 +1,40 @@
+const indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
+
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = ({ target }) => {
+  target.result.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = ({ target }) => {
+  db = target.result;
+
+  // check if app is online before reading from db
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function(event) {
+  console.log("Woops! " + event.target.errorCode);
+};
+
+function saveRecord(record) {
+  // TODO: this function should save a transaction object to indexedDB so that
+  // it can be synced with the database when the user goes back online.
+}
+
+function checkDatabase() {
+  // TODO: this function should check for any saved transactions and post them
+  // all to the database. Delete the transactions from IndexedDB if the post
+  // request is successful.
+}
+
+// listen for app coming back online
+window.addEventListener('online', checkDatabase);
